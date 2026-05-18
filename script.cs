e0b45f2f0c1a2ff1073a64cb9b877645ab41d5c0
yueyinqiu <yueyinqiu@outlook.com>
2026-05-18 12:28:12 +0800

    rm cw

--- script.cs
+++ script.cs
@@ -33,7 +33,6 @@ using var httpClient = new HttpClient();
 
 await Parallel.ForEachAsync(origin.Assets, async (asset, cancellationToken) =>
 {
-    Console.WriteLine($"Converting {asset.Name}...");
     using var stream = await httpClient.GetStreamAsync(asset.BrowserDownloadUrl, cancellationToken);
 
     using var memoryStream = new MemoryStream();
